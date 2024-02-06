06d5c842d64c6af7dfdcecb724985de5fc3f4004
ayushchokhani <101213272+ayushchokhani@users.noreply.github.com>
2024-02-06 21:26:10 +0530

    Update auth.js

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -5,10 +5,7 @@ import {Client, Account, ID} from "appwrite"
 export class AuthService {
     client = new Client()
     account;
-    //we are not making new account here because it is wastage of service we will create it when object is formed
-
-
-    //if we have to change appwrite to any other app(firebase), we just have to make changes to constructor and in try-catch
+    
     constructor() {
         this.client
             .setEndpoint(conf.appwriteUrl)
@@ -16,13 +13,13 @@ export class AuthService {
         this.account = new Account(this.client)
     }
 
-    //using async and await in place of promise
+    
     async createAccount({email, password, name}) {
         try {
             const userAccount = await this.account.create(ID.unique(), email, password, name);
 
             if(userAccount) {
-                //another method --> login of the newly created account
+                
                 return this.login({email, password})
             }
             else {
@@ -42,18 +39,18 @@ export class AuthService {
         }
     }
 
-    //making this method to check if user active or not
+    
     async getCurrentUser() {
         try {
-            //if we are not able to get account then it will also return null
+            
             return await this.account.get();
         } catch (error) {
-            // throw error ---> we can also log error
+            
             console.log("Appwrite service :: getCurrentUser :: error", error);
 
         }
 
-        //if there is problem in try-catch 
+        
         return null;
     }
 
@@ -61,7 +58,7 @@ export class AuthService {
         try {
             await this.account.deleteSessions()
         } catch (error) {
-            // throw error
+            
             console.log('Appwrite service :: logout :: error', error)
         }
     }
@@ -69,5 +66,5 @@ export class AuthService {
 
 const authService = new AuthService();
 
-// we are going to export object directly so that user can access its services directly
+
 export default authService
