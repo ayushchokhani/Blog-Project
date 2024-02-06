87988706dac7ede4f8e2ad9a3afff3a61780d863
ayushchokhani <101213272+ayushchokhani@users.noreply.github.com>
2024-02-06 21:31:46 +0530

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,17 +1,14 @@
 import React from 'react'
 
 function Button({
-    children,  // this children could also have been said as btn text
+    children,  
     type = 'button', 
-    // defining default type, can be changed to submit btn also
     bgColor = 'bg-blue-600',
     textColor = 'text-white',
     className = '',
     ...props
 }) {
   return (
-    // writing our default classname as well as injecting properties passed by user
-    // {...props} ----> any additional property passed by user
     <button className={`px-4 py-2 roudend-lg ${bgColor} 
     ${textColor} ${className}`} {...props}>
         {children}
