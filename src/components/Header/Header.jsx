8dae926048bde3febd7763656646773c5d0d3945
ayushchokhani <101213272+ayushchokhani@users.noreply.github.com>
2024-02-06 21:28:53 +0530

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,15 +7,15 @@ import { useNavigate } from 'react-router-dom'
 
 function Header() {
   const authStatus = useSelector((state) => state.auth.status)
-  // state.auth because it is in authSlice
+  
   const navigate = useNavigate()
 
-  //in production grade generally objects are made in array as it becomes easier to add it to navigation bar
+  
   const navItems = [
-    // we are checking authStatus from above and those who are active we will show them these features otherwise we wil not show them
+  
     {
       name: 'Home',
-      slug: '/', // we can call slug as url too, our choice
+      slug: '/', 
       active: true
     }, 
     {
@@ -45,7 +45,7 @@ function Header() {
       <Container>
         <nav className='flex'>
           <div className='mr-4 '>
-            {/* just wrapping it inside link  */}
+            
             <Link to='/' >
               <Logo width='70px' />
             </Link>
@@ -53,21 +53,18 @@ function Header() {
 
           <ul className='flex ml-auto'>
             {navItems.map((item) => 
-              // if item active then showing something else null
-              // use key for repeating html values
+              
               item.active ? (
                 <li key={item.name}> 
                   <button
-                  // this navigate comes from react-router-dom
-                  // this just takes us to provided url
+                  
                   onClick={() => navigate(item.slug)}
                   className='inline-block px-6 py-2 duration-200 hover: bg-blue-100 rounded-full'
                   >{item.name}</button>
                 </li>
               ) : null
             )}
-            {/* doing below for providing logout btn to user 
-            if user is authenticated then only show logout so using && sign */}
+            
 
             {authStatus && (
               <li>
