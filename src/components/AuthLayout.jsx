4bb2efde20f1f760308776d68fc640f8f1e97a23
ayushchokhani <101213272+ayushchokhani@users.noreply.github.com>
2024-02-06 21:31:18 +0530

    Update AuthLayout.jsx

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -3,22 +3,11 @@ import {useSelector} from 'react-redux'
 import {useNavigate} from 'react-router-dom'
 
 export default function Protected({children, authentication = true}) {
-    // by default authentication is true
     
     const navigate = useNavigate()
     const [loader, setLoader] = useState(true)
     const authStatus = useSelector(state => state.auth.status)
 
-
-    // we can also do it by not taking authentication from user 
-    // if(authStatus === true) {
-    //     navigate('/')
-    // }
-    // else if(authStatus !== true) {
-    //     navigate('/login')
-    // }
-
-    // useEffect is important here
     useEffect(() => {
         if(authentication && authStatus !== authentication) {
             navigate('/login')
@@ -30,7 +19,6 @@ export default function Protected({children, authentication = true}) {
         setLoader(false)
 
     }, [authStatus, navigate, authentication])
-    // we want to check when user navigate from one page to another so navigate is also in dependency array
 
   return loader ? <h1>Loading...</h1> : <>{children}</> 
 }
